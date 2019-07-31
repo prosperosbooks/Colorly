263bff4021a0ce7e7de3fdb7066fd891384835cb
Emin <allen.emin@gmail.com>
2019-07-30 21:36:32 -0400

    refactor color styles

--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -42,7 +42,7 @@ export default class SingleColorPalette extends Component {
         key={color.name}
         name={color.name}
         background={color[format]}
-        showLink={false}
+        showingFullPalette={false}
       />
     ));
 
